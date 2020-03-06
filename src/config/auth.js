49e300ac2ffa77d49c072f8794056275904d492d
pedroaspereira <pedro.apereira9@gmail.com>
2020-03-05 23:17:32 -0300

    JWT implemented

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '146b02add114863b307d4ed677167d66',
+  expiresIn: '7d',
+};
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '',
+  password: 'postgres',
+  port: 5433,
   database: 'supplierx',
   define: {
     timestamps: true,
